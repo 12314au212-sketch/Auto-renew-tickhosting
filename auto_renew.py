93fc3a1abb278e203febb1e713fe65bba9a37a5e
12314au212-sketch <12314au212@gmail.com>
2026-01-20 16:51:04 +0100

    Update auto_renew.py

--- auto_renew.py
+++ auto_renew.py
@@ -120,7 +120,7 @@ def login_to_dashboard(driver):
             (By.NAME, 'username'),  
             (By.ID, 'email'),
             (By.NAME, 'email'),
-            (By.XPATH, "//input[@type='username']"),
+            (By.XPATH, "//input[@type='text']"), #<input type="text" id="username" name="username" placeholder="name@example.com" value="">
         ]
         
         password_selectors = [
